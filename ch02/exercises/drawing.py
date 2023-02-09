0e7475d8dbfd446dbac13465405cc6b81eb52e90
mia wallak <mwallak1@binghamton.edu>
2023-02-08 23:42:53 -0500

    exercise

--- ch02/exercises/drawing.py
+++ ch02/exercises/drawing.py
@@ -1,2 +1,10 @@
-from module import turtle
+import turtle
+t = turtle.Turtle()
 
+sides = int(input("Enter number of sides "))
+length = int(input("Enter length of sides "))
+t.color('pink')
+for i in range(sides):
+    t.forward(length)
+    t.right(90)
+turtle.done()

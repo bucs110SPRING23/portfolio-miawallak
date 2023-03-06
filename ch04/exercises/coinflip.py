285fae4ef193085f09f5bcd46d29653709eda4f3
mia wallak <mwallak1@binghamton.edu>
2023-03-06 16:57:00 -0500

    exercise

---
+++ ch04/exercises/coinflip.py
@@ -0,0 +1,31 @@
+import turtle
+import random
+
+turtle = turtle.Turtle()
+t.shape("turtle")
+t.speed(0)
+screen.setup(width=500, height=500)
+
+
+turtle.penup()
+turtle.goto(0,0)
+turtle.pendown()
+
+def off_screen(turtle):
+    x, y = turtle.position()
+    if abs(x) > 250 or abs(y) > 250:
+        return True
+    else:
+        return False
+
+while not off_screen(turtle):
+    if random.randint(0, 1) == 0:
+        turtle.left(90)
+    else:
+        turtle.right(90)
+
+    for i in range(50):
+        turtle.forward(5)
+
+    if off_screen(turtle):
+        break

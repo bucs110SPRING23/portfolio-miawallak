35997f0810e9052386a17591ff4a38d39abf4d15
mia wallak <mwallak1@binghamton.edu>
2023-03-06 23:04:02 -0500

    lab4 commit

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,20 +1,27 @@
-
 import pygame
 import random
 import math
 pygame.init()
+screen = pygame.display.set_mode()
 
-screen_size = pygame.display.get_window_size()
+
+screen_size = (500, 500);
 screen = pygame.display.set_mode(screen_size)
 
-background_color = (255, 255, 255)
-board_color = (0, 0, 0)
-miss_color = (255, 0, 0)
-hit_color = (0, 255, 0)
+pygame.draw.circle(screen, "blue",(0,0), 100 )
 
-board_radius = min(screen_size) // 2
-board_center = (screen_size[0] // 2, screen_size[1] // 2)
-running = True
+background_color = ("blue")
+board_color = ("pink")
+line_color = ("black")
+miss_color = ("red")
+hit_color = ("green")
+
+screen_size_variable = pygame.display.get_window_size();
+screen_width = screen_size_variable[0];
+screen_height = screen_size_variable[1];
+board_center = (screen_width/2, screen_height/2 );
+board_radius = screen_width/2;
+running = True;
 while running:
  
     for event in pygame.event.get():
@@ -25,32 +32,25 @@ while running:
     screen.fill(background_color)
 
 
-    pygame.draw.circle(screen, board_color, board_center, board_radius, 10)  # outer circle
-    pygame.draw.circle(screen, background_color, board_center, board_radius - 10)  # inner circle
-    for i in range(0, 360, 18):
-        start_pos = (board_center[0] + int(0.8 * board_radius * math.cos(math.radians(i))),
-                     board_center[1] + int(0.8 * board_radius * math.sin(math.radians(i))))
-        end_pos = (board_center[0] + int(board_radius * math.cos(math.radians(i))),
-                   board_center[1] + int(board_radius * math.sin(math.radians(i))))
-        pygame.draw.line(screen, board_color, start_pos, end_pos, 10)  # radial lines
+    pygame.draw.circle(screen, board_color, board_center, board_radius)  
+    pygame.draw.line(screen, line_color, (screen_width / 2, 0), (screen_width / 2, 1000), 5)
+    pygame.draw.line(screen, line_color, (0, screen_height / 2), (1000, screen_height / 2), 5)
 
+    for i in range(10):
+        x1 = random.randrange(0, screen_width)
+        x2 = screen_width/2
+        y1 = random.randrange(0, screen_height)
+        y2 = screen_height/2
+        distance_from_center = math.hypot(x1-x2, y1-y2) 
+        is_in_circle = distance_from_center <= screen_width / 2
 
-    for _ in range(10):
-    
-        dart_x = random.randrange(screen_size[0])
-        dart_y = random.randrange(screen_size[1])
-
-   
-        distance_from_center = math.hypot(dart_x - board_center[0], dart_y - board_center[1])
-        if distance_from_center <= board_radius - 10:
-        
-            pygame.draw.circle(screen, hit_color, (dart_x, dart_y), 2)
+        if is_in_circle:
+            pygame.draw.circle(screen, hit_color, (x1, y1), 5)
         else:
-           
-            pygame.draw.circle(screen, miss_color, (dart_x, dart_y), 2)
-
+            pygame.draw.circle(screen, miss_color, (x1, y1), 5) 
+         
  
     pygame.display.flip()
 
 
-pygame.quit()
+pygame.quit() 

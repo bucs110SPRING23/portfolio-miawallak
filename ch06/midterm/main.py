c8df55c6c4ca4d725818ed95d2b5b566a222774e
mia wallak <mwallak1@binghamton.edu>
2023-03-20 23:33:41 -0400

    midterm

--- ch06/midterm/main.py
+++ ch06/midterm/main.py
@@ -1,35 +1,40 @@
 import turtle
+import random 
 
 t = turtle.Turtle()
 
+main_angle = 90
 special_angle = 45
 
-# Draw the main house structure
+def random_color(): 
+    colors = ["Pink", "purple", "lightgreen", "blue"]
+    return random.choice(colors)
+
 def base(length):
     t.begin_fill()
-    t.color("Pink")
+    t.color(random_color())
     t.forward(length)
-    t.right(90)
+    t.right(main_angle)
     t.forward(length)
-    t.right(90)
+    t.right(main_angle)
     t.forward(length)
-    t.right(90)
+    t.right(main_angle)
     t.forward(length)
     t.end_fill()
-    roof(length)
-    
+    return length
+
 def roof(length):
     t.penup()
     t.goto(0, 0)
     t.pendown()
     t.begin_fill()
-    t.color("green")
+    t.color(random_color())
     t.right(special_angle)
     t.forward(107)
-    t.right(90)
+    t.right(main_angle)
     t.forward(107)
     t.end_fill()
-    door(length)
+    return length 
 
 def door(length):
     t.penup()
@@ -39,11 +44,11 @@ def door(length):
     t.color("Red", "white")
     t.right(special_angle)
     t.forward(60)
-    t.right(90)
+    t.right(main_angle)
     t.forward(40)
-    t.right(90)
+    t.right(main_angle)
     t.forward(60)
-    t.right(90)
+    t.right(main_angle)
     t.forward(40)
     t.end_fill()
 
@@ -52,30 +57,28 @@ def door(length):
     t.pendown()
     t.begin_fill()
     t.color("Red", "white")
-    t.right(90)
+    t.right(main_angle)
     t.forward(60)
-    t.right(90)
+    t.right(main_angle)
     t.forward(40)
-    t.right(90)
+    t.right(main_angle)
     t.forward(60)
-    t.right(90)
+    t.right(main_angle)
     t.forward(40)
     t.end_fill()
-    window(length)
 
 def window(length):
-    # Draw the windows of the house
     t.penup()
     t.goto(20, -20)
     t.color("white", "purple")
     t.pendown()
     t.begin_fill()
     t.forward(40)
-    t.right(90)
+    t.right(main_angle)
     t.forward(50)
-    t.right(90)
+    t.right(main_angle)
     t.forward(40)
-    t.right(90)
+    t.right(main_angle)
     t.forward(50)
     t.end_fill()
     
@@ -85,38 +88,39 @@ def window(length):
     t.pendown()
     t.begin_fill()
     t.forward(50)
-    t.right(90)
+    t.right(main_angle)
     t.forward(40)
-    t.right(90)
+    t.right(main_angle)
     t.forward(50)
-    t.right(90)
+    t.right(main_angle)
     t.forward(40)
     t.end_fill()
-    chimney(length)
    
-# Draw the chimney
 def chimney(length):
     t.penup()
     t.goto(120, 20)
     t.pendown()
     t.begin_fill()
-    t.color("lightgreen")
+    t.color(random_color())
     t.forward(20)
-    t.right(90)
+    t.right(main_angle)
     t.forward(20)
-    t.right(90)
+    t.right(main_angle)
     t.forward(20)
-    t.right(90)
+    t.right(main_angle)
     t.forward(20)
-    t.right(90)
+    t.right(main_angle)
     t.end_fill()
+    return length 
 
-base(150)
-t.end_fill
+def main():
+    base(150)
+    roof(150)
+    door(150)
+    window(150)
+    chimney(150)
+    t.end_fill
 
-# Hide the turtle
+main ()
 t.hideturtle()
-
-# Keep the window open until closed manually
 turtle.done()
-
